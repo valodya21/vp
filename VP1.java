b7606a56481530b57b76d986171380397940f19e
valodya21 <valodya21@gmail.com>
2014-11-11 20:42:35 +0300

    Create VP1.java

---
+++ VP1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vp1;
+
+/**
+ *
+ * @author valodya21
+ */
+public class VP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        FuncXi02 func = new FuncXi02();
+        System.out.println(func.Xi());
+        Laba2 lab = new Laba2();
+        lab.makeFunc();
+        // TODO code application logic here
+    }
+    
+}
