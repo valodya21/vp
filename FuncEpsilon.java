252879ff2a4adacb10d4649673b5d5983863d65e
valodya21 <valodya21@gmail.com>
2014-12-02 20:38:49 +0300

    Update FuncEpsilon.java

--- FuncEpsilon.java
+++ FuncEpsilon.java
@@ -1,20 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package vp1;
 
-/**
- *
- * @author valodya21
- */
+// @author valodya21
+
 public class FuncEpsilon extends FuncG
 {
     public double Epsilon(double lambda, double X, double Y, double t)
     {
-        double answer;
-        answer = Math.cos(lambda*t)*X + Math.sin(lambda*t)*Y;
+        double answer = Math.cos(lambda*t)*X + Math.sin(lambda*t)*Y;
+        value= answer;
         return answer;
     }
     
@@ -23,6 +16,7 @@ public class FuncEpsilon extends FuncG
         double answer=0;
         for(int i=0; i<M; i++)
             answer += Epsilon(lambda[i], X[i], Y[i], t);
+        value= answer;
         return answer;
     }
 }
