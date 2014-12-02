7d0d054be8ec7db181675652ef2b6699feeee0d5
valodya21 <valodya21@gmail.com>
2014-12-02 20:41:36 +0300

    Update FuncG.java

--- FuncG.java
+++ FuncG.java
@@ -1,25 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package vp1;
 
-/**
- *
- * @author valodya21
- */
+//@author valodya21
+
 public class FuncG extends FuncXi02
 {
     public double G(double n, int i)
     {
-        value  = 1.0/Math.pow((1.0+Math.PI*i), 2*n);
+        double answer = 1.0/Math.pow((1.0+Math.PI*i), 2*n);
+        value = answer;
         return value;
     }
     
-    public double G(int i)
-    {
-        return G(2, i); 
-    }
-    
+    public double G(int i){return G(2, i);}
 }
